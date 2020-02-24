a878627615ea76eeca8821170f75e5e5e4259c5a
fernandoxmartin <fernando@DESKTOP-ES6V7UH.localdomain>
2020-02-24 15:56:34 -0800

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,147 +1,133 @@
 import React from "react";
 import "../styles/Projects.scss";
-import ballstats from "../images/nbastat.png";
-import fxmweather from "../images/fxmw.png";
-import guitarpedal from "../images/gp.png";
-import fxmv2 from "../images/fxmweatherv2.png";
-import jobtrackr from "../images/jobtrackr.png";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import proj1a from "../images/1a.png";
+import proj1b from "../images/1b.png";
+import proj2a from "../images/2a.png";
+import proj2b from "../images/2b.png";
+import proj3a from "../images/3a.png";
+import proj3b from "../images/3b.png";
 library.add(fab, faEye);
 
 function Projects() {
   return (
     <div className="project-container" id="projects">
+      <div className="section-title">
+        <h1>Projects </h1>
+        <svg height="5" width="200">
+          <line x1="10" y1="0" x2="200" y2="0" stroke="white" strokeWidth={5} />
+        </svg>
+      </div>
       <div className="project">
         <div className="project-image">
-          <img src={jobtrackr} alt="fxmweather" />
+          <img className="a-img" src={proj1a} alt="jobtrackr" />
+          <img className="b-img" src={proj1b} alt="jobtrackr-mobile" />
         </div>
         <div className="details">
-          <p>
-            A Job Tracking App to stay organized on your job hunt. Implemented
-            with PassportJS for user authentication login/registration.
-          </p>
-          <ul>
-            <li>Ejs / sass</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>Passport</li>
-            <li>MongoDB</li>
-          </ul>
-        </div>
-        <div className="project-links">
-          <a href="https://github.com/fernandoxmartin/job-trackr">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a href="https://trackr-job.herokuapp.com/">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-          </a>
+          <div className="project-title">
+            <h4>Featured Project</h4>
+            <h1>Job Trackr</h1>
+          </div>
+
+          <div className="project-desc">
+            <p>
+              An app to help you stay organized on your job hunt. Add, delete,
+              and update your applications. Implemented with PassportJS and
+              MongoDB for user authentication login/signup.
+            </p>
+          </div>
+          <div className="project-stack">
+            <ul>
+              <li>Node</li>
+              <li>Express</li>
+              <li>Passport</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/fernandoxmartin/job-trackr">
+              <FontAwesomeIcon icon={["fab", "github"]} />
+            </a>
+            <a href="https://trackr-job.herokuapp.com/">
+              <FontAwesomeIcon icon={["fas", "eye"]} />
+            </a>
+          </div>
         </div>
       </div>
       <div className="project">
         <div className="project-image">
-          <img src={fxmv2} alt="fxmweather" />
+          <img className="a-img" src={proj2a} alt="fxmweather" />
+          <img className="b-img" src={proj2b} alt="fxmweather-mobile" />
         </div>
         <div className="details">
-          <p>
-            A React Weather App that fetches JSON data from the OpenWeatherMap
-            API to display the current weather and forecast at the desired
-            location. Search by city name, or Zip Code.
-          </p>
-          <ul>
-            <li>React</li>
-            <li>Sass</li>
-            <li>Node</li>
-            <li>Express</li>
-          </ul>
-        </div>
-        <div className="project-links">
-          <a href="https://github.com/fernandoxmartin/fxm-weather">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a href="https://aqueous-shore-69477.herokuapp.com/">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-          </a>
+          <div className="project-title">
+            <h4>Featured Project</h4>
+            <h1>FXM Weather</h1>
+          </div>
+          <div className="project-desc">
+            <p>
+              A React Weather App that fetches JSON data from the OpenWeatherMap
+              API to display the current weather and forecast at the desired
+              location. Search by city name, or Zip Code.
+            </p>
+          </div>
+          <div className="project-stack">
+            <ul>
+              <li>React</li>
+              <li>Sass</li>
+              <li>Node</li>
+              <li>Express</li>
+            </ul>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/fernandoxmartin/fxm-weather">
+              <FontAwesomeIcon icon={["fab", "github"]} />
+            </a>
+            <a href="https://aqueous-shore-69477.herokuapp.com/">
+              <FontAwesomeIcon icon={["fas", "eye"]} />
+            </a>
+          </div>
         </div>
       </div>
       <div className="project">
         <div className="project-image">
-          <img src={guitarpedal} alt="guitar&pedal" />
+          <img className="a-img" src={proj3a} alt="guitar-pedal" />
+          <img className="b-img" src={proj3b} alt="guitar-pedal-mobile" />
         </div>
         <div className="details">
-          <p>
-            A Fictional Guitar Store built for pedal-heads. Browse through
-            products and build your own combination. Add them to your shopping
-            cart, edit and purchase your gear.
-          </p>
-          <ul>
-            <li>HTML/CSS</li>
-            <li>Javascript</li>
-            <li>Jquery</li>
-          </ul>
-        </div>
-        <div className="project-links">
-          <a href="https://github.com/fernandoxmartin/guitar-and-pedal">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a href="https://fernandoxmartin.github.io/guitar-and-pedal">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-          </a>
-        </div>
-      </div>
-      <div className="project">
-        <div className="project-image">
-          <img src={ballstats} alt="ballstats" />
-        </div>
-        <div className="details">
-          <p>
-            An Application designed to receive and compare NBA player stats.
-            Compare players head-to-head and see the leaders on certain stats.
-            JSON data implemented from BallDontLie API.
-          </p>
-          <ul>
-            <li>HTML</li>
-            <li>SASS</li>
-            <li>Javascript</li>
-          </ul>
-        </div>
-        <div className="project-links">
-          <a href="https://github.com/fernandoxmartin/ballstats">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a href="https://fernandoxmartin.github.io/ballstats">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-          </a>
+          <div className="project-title">
+            <h4>Featured Project</h4>
+            <h1>Guitar & Pedal</h1>
+          </div>
+          <div className="project-desc">
+            <p>
+              A Fictional Guitar Store built for pedal-heads. Browse through
+              products and build your own combination. Add them to your shopping
+              cart, edit and purchase your gear.
+            </p>
+          </div>
+          <div className="project-stack">
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>Jquery</li>
+            </ul>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/fernandoxmartin/guitar-and-pedal">
+              <FontAwesomeIcon icon={["fab", "github"]} />
+            </a>
+            <a href="https://fernandoxmartin.github.io/guitar-and-pedal/">
+              <FontAwesomeIcon icon={["fas", "eye"]} />
+            </a>
+          </div>
         </div>
       </div>
-      <div className="project">
-        <div className="project-image">
-          <img src={fxmweather} alt="fxmweather" />
-        </div>
-        <div className="details">
-          <p>
-            A Weather App that fetches JSON data from the OpenWeatherMap API to
-            display the current information at the desired location. Search by
-            city name, or Zip Code.
-          </p>
-          <ul>
-            <li>HTML/CSS</li>
-            <li>Javascript</li>
-            <li>Node</li>
-            <li>Express</li>
-          </ul>
-        </div>
-        <div className="project-links">
-          <a href="https://github.com/fernandoxmartin/Node-Weather-App">
-            <FontAwesomeIcon icon={["fab", "github"]} />
-          </a>
-          <a href="https://fxmweather.herokuapp.com">
-            <FontAwesomeIcon icon={["fas", "eye"]} />
-          </a>
-        </div>
-      </div>
+
       <div className="more-projects">
         <a href="https://github.com/fernandoxmartin">View More Projects</a>
       </div>
